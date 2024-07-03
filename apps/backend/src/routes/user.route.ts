09052cf8ac184001b22331e8b28c9ec4d1473ec9
krabhi1 <abhishek33gl@gmail.com>
2024-07-03 23:45:35 +0530

    Refactor user.route.ts to handle Google sign-in and access token refresh

--- apps/backend/src/routes/user.route.ts
+++ apps/backend/src/routes/user.route.ts
@@ -1,52 +1,62 @@
-
-import { Router } from 'express';
-import { ResultCode, makeErrorResult, makeResult, makeResultFrom } from '../others/utils.js';
-import { handleGetUser, handleGoogleSignInFromCode } from '../services/user.services.js';
-import { refreshUserSignInToken } from '../jwt/user.jwt.js';
-import { auth } from '../others/routes.utils.js';
-import { decodeJWT } from '../others/jwt.utils.js';
+import { Router } from "express";
+import {
+  ResultCode,
+  makeErrorResult,
+  makeResult,
+  makeResultFrom,
+} from "../others/utils.js";
+import {
+  handleGetUser,
+  handleGoogleSignInFromCode,
+  handleRefreshAccessToken,
+} from "../services/user.services.js";
+import { refreshUserSignInToken } from "../jwt/user.jwt.js";
+import { auth } from "../others/routes.utils.js";
+import { decodeJWT } from "../others/jwt.utils.js";
 
 const userRouter: Router = Router();
 
-userRouter.post('/google/signin', async (req, res) => {
-    const { code } = req.body;
-    const result = makeErrorResult<{ token: string }>();
-    if (!code) {
-        const result = makeResult({ code: ResultCode.HTTP_BAD_REQUEST, message: 'Missing code' });
-        return res.status(result.code).json(result);
-    }
-    const tResult = await handleGoogleSignInFromCode(code);
-    if (!tResult.data) {
-        return res.status(400).json(tResult);
-    }
-    makeResultFrom({ other: tResult, result })
-    res.send(result);
-})
+userRouter.post("/google/signin", async (req, res) => {
+  const { code } = req.body;
+  const result = makeErrorResult<{ token: string }>();
+  if (!code) {
+    const result = makeResult({
+      code: ResultCode.HTTP_BAD_REQUEST,
+      message: "Missing code",
+    });
+    return res.status(result.code).json(result);
+  }
+  const tResult = await handleGoogleSignInFromCode(code);
+  if (!tResult.data) {
+    return res.status(400).json(tResult);
+  }
+  makeResultFrom({ other: tResult, result });
+  res.send(result);
+});
 
-
-userRouter.post('/refresh/access_token', async (req, res) => {
-    const { token } = req.body;
-    if (!token) {
-        const result = makeResult({ code: ResultCode.HTTP_BAD_REQUEST, message: 'Missing token' });
-        return res.status(result.code).json(result);
-    }
-    console.log("s----s",decodeJWT(token))
-    const tResult = refreshUserSignInToken(token);
-    if (!tResult.data) {
-        return res.status(400).json(tResult);
-    }
-    res.send(tResult);
-})
-
-
+userRouter.post("/refresh/access_token", async (req, res) => {
+  const { token } = req.body;
+  if (!token) {
+    const result = makeResult({
+      code: ResultCode.HTTP_BAD_REQUEST,
+      message: "Missing token",
+    });
+    return res.status(result.code).json(result);
+  }
+  const tResult = await handleRefreshAccessToken(token);
+  if (!tResult.data) {
+    return res.status(400).json(tResult);
+  }
+  res.send(tResult);
+});
 
 //profile
-userRouter.get('/profile', auth, async (req, res) => {
-    const userId = req.userId;
-    const result = await handleGetUser(userId)
-    if (!result.data) {
-        return res.status(400).json(result)
-    }
-    res.send(result)
-})
+userRouter.get("/profile", auth, async (req, res) => {
+  const userId = req.userId;
+  const result = await handleGetUser(userId);
+  if (!result.data) {
+    return res.status(400).json(result);
+  }
+  res.send(result);
+});
 export default userRouter;
--- apps/backend/src/services/user.services.ts
+++ apps/backend/src/services/user.services.ts
@@ -1,102 +1,91 @@
 import { createUser, getUsers } from "../database/user.db.js";
-import { generateUserSignInToken, refreshUserSignInToken, verifyUserSignInToken } from "../jwt/user.jwt.js";
+import {
+  generateUserSignInToken,
+  refreshUserSignInToken,
+  verifyUserSignInToken,
+} from "../jwt/user.jwt.js";
 import { getGoogleTokens, getUserProfile } from "../others/google.utils.js";
-import { ResultCode, makeResultFrom, makeResult, makeErrorResult } from "../others/utils.js";
+import {
+  ResultCode,
+  makeResultFrom,
+  makeResult,
+  makeErrorResult,
+} from "../others/utils.js";
 import { GetUser } from "../types/user.types.js";
 
-
-
 export async function handleGoogleSignInFromCode(code: string) {
-    let result = makeErrorResult<{ token: string }>();
-    const tokensResult = await getGoogleTokens(code);
-    if (!tokensResult.data) {
-        return makeResultFrom({
-            other: tokensResult,
-            result
-        })
+  let result = makeErrorResult<{ token: string }>();
+  const tokensResult = await getGoogleTokens(code);
+  if (!tokensResult.data) {
+    return makeResultFrom({
+      other: tokensResult,
+      result,
+    });
+  }
+
+  const profileResult = await getUserProfile(tokensResult.data.access_token);
+  if (!profileResult.data) {
+    return makeResultFrom({
+      other: profileResult,
+      result,
+    });
+  }
+
+  //check user is new else create
+  const gResult = await getUsers({ email: profileResult.data.email });
+  if (!gResult.data || gResult.data.length == 0) {
+    if (!tokensResult.data.refresh_token) {
+      return makeResultFrom({
+        code: ResultCode.GOOGLE_MISSING_REFRESH_TOKEN,
+        result,
+      });
     }
-
-    const profileResult = await getUserProfile(tokensResult.data.access_token);
-    if (!profileResult.data) {
-        return makeResultFrom({
-            other: profileResult,
-            result
-        })
+    const cResult = await createUser({
+      email: profileResult.data.email,
+      name: profileResult.data.name,
+      picUrl: profileResult.data.picture,
+      googleRefreshToken: tokensResult.data.refresh_token!!,
+    });
+    if (!cResult.data) {
+      return makeResultFrom({
+        other: cResult,
+        result,
+      });
     }
-
-    //check user is new else create
-    const gResult = await getUsers({ email: profileResult.data.email });
-    if (!gResult.data || gResult.data.length == 0) {
-        if (!tokensResult.data.refresh_token) {
-            return makeResultFrom({
-                code: ResultCode.GOOGLE_MISSING_REFRESH_TOKEN, result
-            })
-        }
-        const cResult = await createUser({
-            email: profileResult.data.email,
-            name: profileResult.data.name,
-            picUrl: profileResult.data.picture,
-            googleRefreshToken: tokensResult.data.refresh_token!!
-        })
-        if (!cResult.data) {
-            return makeResultFrom({
-                other: cResult,
-                result
-            })
-        }
-
-
-    }
-    else {
-        //getting new token
-        const token = generateUserSignInToken({ userId: gResult.data[0].id });
-        result = {
-            code: ResultCode.OK,
-            data: {
-                token: token
-            }
-        }
-    }
-    return result;
-
+  } else {
+    //getting new token
+    const token = generateUserSignInToken({ userId: gResult.data[0].id });
+    result = {
+      code: ResultCode.OK,
+      data: {
+        token: token,
+      },
+    };
+  }
+  return result;
 }
 
-
 export async function handleRefreshAccessToken(oldToken: string) {
-    const result = makeErrorResult<{ token: string }>();
-    const vResult = refreshUserSignInToken(oldToken);
-    if (!vResult.data) {
-        return makeResultFrom({
-            other: vResult,
-            result
-        })
-    }
-    const token = generateUserSignInToken({ userId: vResult.data.userId });
-    result.code = ResultCode.OK;
-    result.data = {
-        token: token
-    }
-
-    return result;
+  return refreshUserSignInToken(oldToken);
 }
 
 export async function handleGetUser(userId: string) {
-    const result = makeResult<GetUser>();
-    const gResult = await getUsers({ id: userId })
-    if (!gResult.data || gResult.data.length == 0) {
-        return makeResultFrom({
-            other: gResult,
-            result
-        })
-    }
-    result.code = ResultCode.OK
-    const user = gResult.data[0]
-    result.data = {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        picUrl: user.picUrl,
-        createdAt: user.createdAt
-    }
-    return result
+  const result = makeResult<GetUser>();
+  const gResult = await getUsers({ id: userId });
+  if (!gResult.data || gResult.data.length == 0) {
+    return makeResultFrom({
+      other: gResult,
+      result,
+    });
+  }
+  result.code = ResultCode.OK;
+  const user = gResult.data[0];
+  result.data = {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    picUrl: user.picUrl,
+    createdAt: user.createdAt,
+  };
+  return result;
 }
