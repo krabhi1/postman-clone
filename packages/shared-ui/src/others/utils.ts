6bce4e5662d2ba3153a28176f76ecbb05bf8ab34
krabhi1 <abhishek33gl@gmail.com>
2024-07-14 04:47:22 +0530

    chore: Update Google redirect URL in env.config.ts and related files

--- packages/shared-ui/src/others/utils.ts
+++ packages/shared-ui/src/others/utils.ts
@@ -6,18 +6,20 @@ import { memo } from "react";
 export function loadViteEnv() {
   let _env = {
     GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID as string,
-    GOOGLE_REDIRECT_URI: import.meta.env.VITE_GOOGLE_REDIRECT_URI as string,
+    GOOGLE_REDIRECT_URL: import.meta.env.VITE_GOOGLE_REDIRECT_URL as string,
     SERVER_URL: import.meta.env.VITE_SERVER_URL as string,
   };
   updateEnv(_env);
+console.log({ _env });
+
 }
 
-export function loadResourceForApp(){
+export function loadResourceForApp() {
   loadViteEnv();
   setRouter(createBrowserRouter(routes));
 }
 
-export type ReactChildren={
+export type ReactChildren = {
   children?: React.ReactNode;
 }
 
